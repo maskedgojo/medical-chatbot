165498c31fff5be4f1c369c3d585c2aa847bb7bc
Kushaagra <maskedgojo2004@gmail.com>
2025-03-19 21:34:32 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'maskedgojo',
+    author_email= 'kushaagra27op@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
